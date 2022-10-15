538754fa0da5c7e4ee6b8f001e5f83033399149c
Vishal Goel <vishugoel2812@gmail.com>
2022-10-16 00:57:47 +0530

    time inside clock is updated

--- ind.js
+++ ind.js
@@ -1,30 +1,59 @@
 // window.addEventListener("load",function(){
+    var date1= new Date();
+    let d=date1.getSeconds();
+    d*=6;
+    let b1=date1.getMinutes()%60;
+    b1*=6;
+    let c1=date1.getHours()%12;
+    c1*=6;
+    var time = document.getElementById("time");
     let secondAxis=document.getElementById("secAxis");
     let minAxis=document.getElementById("minAxis");
     let hourAxis=document.getElementById("hourAxis");
-    minAxis.style.transform="rotate(30deg)";
-    let a=0,b=30,c=0;
-    var date;
+    secAxis.style.transform="rotate("+(d*6)+"deg)";
+    minAxis.style.transform="rotate("+(b1)+"deg)";
+    hourAxis.style.transform="rotate("+(c1)+"deg)";
+    // let a=0,b=0,c=0;
+    // var date;
+    var q=0;
     setInterval(function(){
-    secondAxis.style.transform="rotate("+a+"deg)";
-    if(a%360==0 && a!=0){
-    minAxis.style.transform="rotate("+b+"deg)";
-    b+=30;
+        var date= new Date();
+        let m=date.getSeconds();
+        var k;
+        if(d==360){
+            d=0;
+            // flag=false;
+            q++;
+         k=360;
+        }else{
+            k=0;
+        }
+    secondAxis.style.transform="rotate("+(m*6)+"deg)";
+
+    if(k==360){
+        b1+=6;
+    minAxis.style.transform="rotate("+b1+"deg)";
     }
-    if(a%(360*60)==0 && a!=0){
-    hourAxis.style.transform="rotate("+c+"deg)";
-    c+=30;
+    if(q==60){
+        c1+=30;
+    hourAxis.style.transform="rotate("+c1+"deg)";
+    q=0;
     }
-    a+=6;
-    date= new Date();
-    var a=date.getSeconds();
-    var b=date.getMinutes();
-    var c=date.getHours();
+
+    d+=6;
+    let n=date.getMinutes();
+    let o=date.getHours();
     var time = document.getElementById("time");
-    if(a<10){
-        a="0"+a;
+    if(m<10){
+        m="0"+m;
     }
-    time.innerHTML="<h1 id='time' style:'text-align:center'>"+c+" : "+b+" : "+a+"</h1>";
+    if(n<10){
+        n="0"+n;
+    }
+    if(o<10){
+        o="0"+o;
+    }
+    time.innerHTML="<h1 id='time' style:'text-align:center'>"+o+" : "+n+" : "+m+"</h1>";
     },1000)
 
 // })
