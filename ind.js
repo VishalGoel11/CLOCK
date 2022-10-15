761f1e38067bf2fbaa143c21abb26e3527747004
Vishal Goel <vishugoel2812@gmail.com>
2022-10-15 21:48:56 +0530

    timer event added

--- ind.js
+++ ind.js
@@ -4,6 +4,7 @@
     let hourAxis=document.getElementById("hourAxis");
     minAxis.style.transform="rotate(30deg)";
     let a=0,b=30,c=0;
+    var date;
     setInterval(function(){
     secondAxis.style.transform="rotate("+a+"deg)";
     if(a%360==0 && a!=0){
@@ -15,6 +16,15 @@
     c+=30;
     }
     a+=6;
-    
+    date= new Date();
+    var a=date.getSeconds();
+    var b=date.getMinutes();
+    var c=date.getHours();
+    var time = document.getElementById("time");
+    if(a<10){
+        a="0"+a;
+    }
+    time.innerHTML="<h1 id='time' style:'text-align:center'>"+c+" : "+b+" : "+a+"</h1>";
     },1000)
+
 // })
